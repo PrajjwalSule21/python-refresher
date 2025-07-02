7e761fefbacf70d091deed0b93524920f7f029b6
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-02 22:04:52 +0530

    Python Vairables

---
+++ 01_Variables/vairable.py
@@ -0,0 +1,28 @@
+"""
+variables -> Think as a container which store anything, number, characters, sentences and manu more.
+"""
+
+cost = 21
+tax_percentage = .25
+tax = cost * tax_percentage
+price = cost + tax
+
+print(price)
+
+
+firstName = "Prajjwal"
+lastName = "Sule"
+
+print(firstName, lastName)
+
+
+fullName = firstName + ' ' + lastName
+print(fullName)
+
+
+a = 21
+b = 12
+print(a)
+
+a = 31
+print(a)
