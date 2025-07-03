a73509b35b3601a689cfe4116dc71356c26fa562
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-03 17:25:45 +0530

    Python Boolean and Operators

---
+++ 07_BooleanandOperators/boolean_operator.py
@@ -0,0 +1,48 @@
+'''
+Boolean and Operator
+'''
+
+like_coffee = True
+like_tea = False
+
+print(like_tea, like_coffee)
+
+favorite_food = 'Pizza'
+favorite_number = 21
+
+print(type(favorite_food))
+print(type(favorite_number))
+print(type(like_tea))
+
+
+# Comparison Operators
+
+# == (equals to/comparison)
+
+print(1==2) # -> False
+
+# != (not equals)
+
+print(1!=2) # -> True
+
+
+print(1>2) # False
+print(1<2) # True
+
+
+print(1 >= 1)
+print(1 <= 2)
+
+
+# Logical Operators
+
+print(1>3 and 5<7) # check for both conditions must be true
+
+print(1<3 and 5 <7)
+
+
+print(1>3 or 5<7) #any one of them must be true
+
+print(not(1==1))
+
+print(not(1==2))
