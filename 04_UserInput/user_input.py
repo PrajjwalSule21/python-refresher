987936f00e9362680f9ee5a8569b4ead57555867
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-03 10:56:48 +0530

    Python User Input

---
+++ 04_UserInput/user_input.py
@@ -0,0 +1,8 @@
+"""
+User Input : this will take input from user before running the entire code
+"""
+
+first_name = input("Enter your first name: ")
+days = input("How many days before your birthday: ")
+print(f'Hi {first_name}, only {days} days '
+      f'before your Birthday!')
