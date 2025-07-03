66afe50b28537446820466060e168aac249fb417
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-03 17:35:42 +0530

    Python If Else Elif

---
+++ 08_IfElseElif/if_elif_else.py
@@ -0,0 +1,47 @@
+'''
+if else elif
+This is known as conditional statements and known for flow statement
+'''
+
+
+
+x = 1
+if x == 1:
+    print('x is 1')
+print("This line will run all the time no matter of condition")
+
+
+b = 21
+if b > 10:
+    print('B is greater than 21')
+else:
+    print('B is not greater then 21')
+
+
+
+hour = 13
+
+if hour < 15:
+    print('Good Morning')
+else:
+    print("Good Evening")
+
+
+
+hour = 16
+
+if hour < 15:
+    print('Good Morning')
+else:
+    print("Good Evening")
+
+
+name = "prajjwal"
+
+if name == "karl":
+    print('name is karl')
+elif name == 'Joe':
+    print('name is joe')
+else:
+    print('name is prajjwal')
+
