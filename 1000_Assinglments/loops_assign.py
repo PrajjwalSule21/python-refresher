f802ea9ae2ba4684c663fa2a0d3955dc08cfcbbf
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-03 19:40:24 +0530

    loop assignment

---
+++ 1000_Assinglments/loops_assign.py
@@ -0,0 +1,20 @@
+"""
+Given: my_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+- Create a while loop that prints all elements of the my_list variable 3 times.
+
+- When printing the elements, use a for loop to print the elements
+
+- However, if the element of the for loop is equal to Monday, continue without printing
+"""
+
+
+my_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+num = 0
+while num < 3:
+    num = num + 1
+    for i in my_list:
+        if i == 'Monday':
+            continue
+        print(i)
