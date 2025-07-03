768e792f26eaf75b7bc835dfd18df5dafe1ecaeb
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-03 10:05:54 +0530

    python comments

---
+++ 02_Comments/coments.py
@@ -0,0 +1,19 @@
+"""
+You can treat comments as definition or logs of your code logic, it can also use to gave idea about code to other developer.
+"""
+
+
+print("Hello World!") # This will print Hello World!
+
+# print('Hello World') this is entirely commented and can't print anythin
+
+
+# This is use for single line comments
+
+"""
+This is for mutliple line comments
+"""
+
+'''
+As this is also used to do multiline comments
+'''
